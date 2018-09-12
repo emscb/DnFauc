e33fb1b4c179b834d111297a4a6194ca4b769290
EMSCB\emscb <khmzxc@naver.com>
2018-09-12 19:59:08 +0900

    1809121959

--- set_jonadan.py
+++ set_jonadan.py
@@ -7,32 +7,47 @@ except:
     print("목록파일이 없습니다.")
 
 itemList = []
-
+priceList = []
+itemLimitList = {}
+calcList = []
 
 for i in w.readlines():
     x, y = i.split('-')[0], i.split('-')[1]
-    y = int(y)
+    y = float(y)
     itemList.append((x,y))
+    itemLimitList[x] = int(i.split('-')[2])
 
 for i in itemList:
     a = auc(i[0])
     a.crawl()
-    priceList = a.process()
+    aucList = a.process()
     price = 0
     num = i[1]
-    for j in priceList:
+    for j in aucList:
         if j[1] <= num:
             num -= j[1]
             price += j[0]*j[1]
         else:
             price += j[0]*num
             break
-    # 정테를 살 경우 101380
-    # 캔 정테로 살 경우 81110
+    # 정테를 살 경우 개당 101380
+    # 캔 정테로 살 경우 개당 81110
     if price > 101380:
-        print(i[0] + "는 테라를 사서라도 정테로 사세요.")
+        print(i[0] + "는 테라를 사서라도 정테로 사세요."); priceList.append((i[0],price))
     elif price > 81110:
-        print(i[0] + "는 테라를 캐서 테라로 사세요.")
+        print(i[0] + "는 테라를 캐서 테라로 사세요."); priceList.append((i[0],price))
     else:
         print(i[0] + "는 경매장에서 사세요.")
+print()
+
+priceList.sort(key=lambda x: x[1], reverse=True)
+for i in priceList:
+    for j in range(itemLimitList[i[0]]):
+        calcList.append(i[0])
+
+r=0
+while(r<10):
+    print(calcList[r] + "사세요.")
+    r += 1
+
 time.sleep(20)
