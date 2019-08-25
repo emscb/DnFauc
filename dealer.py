a41296f90bf4f3541aaf9766a2a336c01bf0a482
Hyeok Min Kwon <khmzxc@naver.com>
2019-08-25 11:42:48 +0900

    딜러용 물약 가격 비교

--- dealer.py
+++ dealer.py
@@ -6,6 +6,35 @@ priceList = {}
 for m in matList.keys():
     a = Auc(m)
     l = a.crawl()
+    count = 500
+    amount = 0
+    # 500개 사면 개당 얼만지 priceList에 추가
     for l1 in l:
-        print(l1['count'])
-        print(l1['unitPrice'])
+        if l1['count'] >= count:
+            amount += (l1['unitPrice'] * count)
+            count = 0
+        else:
+            amount += l1['unitPrice'] * l1['count']
+            count -= l1['count']
+            continue
+        priceList[m] = amount / 500
+        print(m + "의 값이 저장되었습니다.")
+        break
+    if count > 0:
+        priceList[m] = amount / count
+        print(m + "의 값이 저장되었습니다.")
+thp = Auc("투신의 함성 포션").crawl()[0]['unitPrice']
+jjg = Auc("정신 자극의 비약").crawl()[0]['unitPrice']
+
+print()
+
+if thp >= priceList["전사의 함성 포션"] + priceList["무색 큐브 조각"] * 60:
+    print("투함포는 만드세요.")
+else:
+    print("투함포는 경매장에서 사세요.")
+
+if jjg >= priceList["생명의 숨결"]*10 + priceList["청색 마력의 산물"]*10:
+    print("정자극은 만드세요.")
+else:
+    print("정자극은 경매장에서 사세요.")
+time.sleep(10)
