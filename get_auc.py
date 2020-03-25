ff9a8b3a054bfe48bf1cf56186b969cdd19c21be
Hyeok Min Kwon <khmzxc@naver.com>
2020-03-25 14:17:21 +0900

    동작 과정 수정

--- get_auc.py
+++ get_auc.py
@@ -10,24 +10,27 @@ try:
     conn = sqlite3.connect(db_path)
     c = conn.cursor()
     itemList_table = c.execute("SELECT itemName FROM item")
+
+    for i in itemList_table:
+        i = list(i)[0]
+        itemList.append(i)
+
+    for k in itemList:
+        obj = Auc(k)
+        if obj.status:
+            itemObj.append(obj)
+        else:
+            exit(-1)
+
 except sqlite3.OperationalError:  # 테이블 없음
     print('DB가 경로상에 없습니다.')
 
-for i in itemList_table:
-    i = list(i)[0]
-    itemList.append(i)
-
-for k in itemList:
-    itemObj.append(Auc(k))
-
 for j in itemObj:
     price = j.get_avgPrice()
+    if price == -1:
+        exit(-1)
     sq = '''INSERT INTO aucInfo VALUES(?,?,?,?)'''
-    try:
-        value = tuple(price[0])
-    except:
-        print("%s의 가격 정보가 이상해서 넘어갑니다." % j.itemname)
-        continue
+    value = tuple(price[0])
     rm = c.execute(sq, value)
     conn.commit()
     print(time.ctime(), j.itemname, '\b의 가격이 입력되었습니다.')
--- set_auc.py
+++ set_auc.py
@@ -8,6 +8,10 @@ class Auc:
         self.limit = 5
         self.url = 'https://api.neople.co.kr/df/auction?itemName=' + itemname + \
                    '&limit=' + str(self.limit) + '&sort=unitPrice:asc&apikey=nJeolB5EWc0nUNTYk62nFcPH3e9L9WJG'
+        self.status = False
+        self.registered_list = self.crawl()
+        if self.registered_list != -1:
+            self.status = True
 
     # 처리된 row list 반환
     def crawl(self):
@@ -23,32 +27,31 @@ class Auc:
             return -1
         if len(k['rows']) == 0: # 결과가 없는 경우
             print('등록된 아이템이 없거나 경매장에 등록할 수 없는 아이템입니다.')
-            return -1
+            return []
         else:
             return k['rows']
 
     def get_avgPrice(self):
-        self.price = []
+        if not self.registered_list:
+            return -1
+
+        price = []
 
         # 어제의 평균가이기 때문에 어제 요일을 찾음
         t = time.localtime()
         if t.tm_hour <= 5:
             t = time.localtime(time.time()-86400.0*2.0)
-            self.now = '%04d-%02d-%02d' % (t.tm_year, t.tm_mon, t.tm_mday)
+            now = '%04d-%02d-%02d' % (t.tm_year, t.tm_mon, t.tm_mday)
         else:
             t = time.localtime(time.time()-86400.0)
-            self.now = '%04d-%02d-%02d' % (t.tm_year, t.tm_mon, t.tm_mday)
+            now = '%04d-%02d-%02d' % (t.tm_year, t.tm_mon, t.tm_mday)
         wday = {0: '월', 1: '화', 2: '수', 3: '목', 4: '금', 5: '토', 6: '일'}
 
-        registered_list = self.crawl()
-        if registered_list == -1:
-            return -1
-
         try:
-            for i in registered_list:
+            for i in self.registered_list:
                 if i['unitPrice'] != 0:
-                    self.price.append([self.now, wday[t.tm_wday], i['itemName'], i['averagePrice']])
+                    price.append([now, wday[t.tm_wday], i['itemName'], i['averagePrice']])
         except KeyError:  # unitPrice가 없으면 평균가가 적혀있지 않음
             print('즉시 구매 가능한 아이템이 없어 넘어갑니다.')
             return -1
-        return self.price
+        return price
