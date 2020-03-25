19e7b53e36541cf88b71e08e80f377b9f9d7e665
Hyeok Min Kwon <khmzxc@naver.com>
2020-03-25 14:59:23 +0900

    검색 방법 변경 (full->match)

--- set_skill.py
+++ set_skill.py
@@ -3,7 +3,7 @@ import requests
 
 class skill:
     def __init__(self, name):
-        self.url = 'https://api.neople.co.kr/df/items?itemName=' + name + '&limit=30&wordType=full&apikey=nJeolB5EWc0nUNTYk62nFcPH3e9L9WJG'
+        self.url = 'https://api.neople.co.kr/df/items?itemName=' + name + '&limit=30&wordType=match&apikey=nJeolB5EWc0nUNTYk62nFcPH3e9L9WJG'
 
     def listing(self):
         self.idList = []
