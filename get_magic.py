a88f3faba54226ff5babab7267824b50e55eec0e
Hyeok Min Kwon <khmzxc@naver.com>
2020-04-08 17:03:47 +0900

    Fix lint

--- get_magic.py
+++ get_magic.py
@@ -8,7 +8,7 @@ matPrice2 = {}
 for m in material:
     a = auc(m)
     a.crawl()
-    
+
     # 현재 1000개 매입가로 계산
     priceList = a.process()
     c = calculate()
@@ -22,13 +22,13 @@ print(matPrice)
 print(matPrice2)
 
 # 기준 계산
-strd = (matPrice['생명의 숨결']*28 + matPrice['용의 심장']*9 + matPrice['황금 가루']*9 + matPrice['하급 원소결정']*9
-+ matPrice['두툼한 고기']*9)/4.0
-strd2 = (matPrice2['생명의 숨결']*28 + matPrice2['용의 심장']*9 + matPrice2['황금 가루']*9 + matPrice2['하급 원소결정']*9
-+ matPrice2['두툼한 고기']*9)/4.0
+strd = (matPrice['생명의 숨결'] * 28 + matPrice['용의 심장'] * 9 + matPrice['황금 가루'] * 9 + matPrice['하급 원소결정'] * 9
+        + matPrice['두툼한 고기'] * 9) / 4.0
+strd2 = (matPrice2['생명의 숨결'] * 28 + matPrice2['용의 심장'] * 9 + matPrice2['황금 가루'] * 9 + matPrice2['하급 원소결정'] * 9
+         + matPrice2['두툼한 고기'] * 9) / 4.0
 
-print('현재 매입가 기준 : ' + str(strd*97/100))
-print('어제 평균가 기준 : ' + str(strd2*97/100))
+print('현재 매입가 기준 : ' + str(strd * 97 / 100))
+print('어제 평균가 기준 : ' + str(strd2 * 97 / 100))
 
 T = Telegram('@dnfdodododo')
-T.send('현재 매입가 기준 : ' + str(int(strd*97/100)) + '\n' + '어제 평균가 기준 : ' + str(int(strd2*97/100)))
+T.send('현재 매입가 기준 : ' + str(int(strd * 97 / 100)) + '\n' + '어제 평균가 기준 : ' + str(int(strd2 * 97 / 100)))
