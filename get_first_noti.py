361794a756c41fc9308d4011f991b3aba2cce090
Hyeok Min Kwon <khmzxc@naver.com>
2020-04-22 10:18:56 +0900

    Add console print

--- get_first_noti.py
+++ get_first_noti.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 T = Telegram('@dnfdodododo')
 trs = []
 while 1:
+    print("%s 홈페이지 스캔 중..." % str(time.ctime()))
     try:
         response = requests.get("http://df.nexon.com/df/testinfo/notice")
     except requests.exceptions.ConnectionError:
@@ -16,6 +17,7 @@ while 1:
         print("Error code : {code}".format(code=response.status_code))
         continue
     else:
+        print("스캔 완료!")
         soup = BeautifulSoup(response.text, 'html.parser')
         tr = soup.select('table.tbl > tbody > tr > td > a')
         if not trs:
@@ -25,5 +27,6 @@ while 1:
             for t in tr:
                 if t.text not in trs:
                     trs.append(t.text)
+                    print("새로운 공지 발견!")
                     T.send("<퍼섭 공지사항>\n{title}".format(title=t.text))
     time.sleep(10 * 60)
